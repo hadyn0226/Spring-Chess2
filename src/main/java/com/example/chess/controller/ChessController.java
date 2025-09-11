085f3168b9e1d871ebe138b8c49f91595ff2d81e
DESKTOP-V3THOI2\hadyn <durtlehdduq@gmail.com>
2025-09-11 21:29:07 +0900

    edited Matching to WebSocekt

--- src/main/java/com/example/chess/controller/ChessController.java
+++ src/main/java/com/example/chess/controller/ChessController.java
@@ -1,19 +1,44 @@
 package com.example.chess.controller;
 
+import java.util.List;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import com.example.chess.dto.ChatMessage;
+import com.example.chess.dto.MatchRequest;
+import com.example.chess.dto.MatchResponse;
+import com.example.chess.service.MatchService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class ChessController {
+	
+	private final MatchService matchService;
+	private final SimpMessagingTemplate messagingTemplate;
 	
 	@MessageMapping("sendMessage")
 	@SendTo("/topic/messages")
 	public ChatMessage send(ChatMessage chatMessage) {
 		System.out.println(chatMessage);
 		return chatMessage;
+	}
+	
+	@MessageMapping("/match")
+	public void match(MatchRequest request) {
+		log.info("Matching Request!");
+		MatchResponse response = matchService.addToQueue(request.getUserId());
+		if("MATCHED".equals(response.getStatus())) {
+			for(String userId : matchService.getUserIds(response.getGameId())) {
+				messagingTemplate.convertAndSend("/topic/match/" + userId, response);
+			}
+		}
 	}
 	
 }
--- src/main/java/com/example/chess/service/MatchService.java
+++ src/main/java/com/example/chess/service/MatchService.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 
 import com.example.chess.dto.MatchResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class MatchService {
 	
@@ -25,17 +28,27 @@ public class MatchService {
             }
         }
 
-        // 대기열에 유저 추가
-        if (waitingQueue.isEmpty()) {
-            waitingQueue.add(userId);
-            return new MatchResponse("WAIT", null);
-        } else {
-            String opponentId = waitingQueue.poll();
-            String gameId = UUID.randomUUID().toString();
-            games.put(gameId, List.of(opponentId, userId));
-            System.out.println("Matched!");
-            return new MatchResponse("MATCHED", gameId);
-        }
+        return matchPlayer(userId);
+       
     }
+    
+    public List<String> getUserIds(String gameId) {
+    	return games.get(gameId);
+    }
+    
+    // 대기열에 유저 추가
+    private MatchResponse matchPlayer(String userId) {
+    	 if (waitingQueue.isEmpty()) {
+             waitingQueue.add(userId);
+             return new MatchResponse("WAIT", null);
+         } else {
+             String opponentId = waitingQueue.poll();
+             String gameId = UUID.randomUUID().toString();
+             games.put(gameId, List.of(opponentId, userId));
+             log.info("Matched! gameId={}, players={}", gameId, List.of(opponentId, userId));
+             return new MatchResponse("MATCHED", gameId);
+         }
+    }
+    	
 	
 }
